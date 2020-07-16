4a895053c93e1bffa4d0371b7a8febdae8d3ec2c
aidanakad <aydana0504@gmail.com>
2020-07-16 12:50:44 +0600

    render guide info in the fynamic page

--- src/Components/ForTourist/index.js
+++ src/Components/ForTourist/index.js
@@ -7,26 +7,42 @@ export default function ForTourist ({match:{params: {id}}}){
     
 
     // const id = parseInt(id)
-    const [list, setList] = useState([])
+    const [guide, setGuide] = useState(null)
     useEffect(()=>{
-        fetch(`http://localhost:1717/${id}`)
+        fetch(`http://localhost:1717/guides/${id}`)
         .then(response=>{
             if(!response.ok) throw new Error('guide not found')
             return response.json()
         }) 
         .then((data)=>{
-            setList(data)
+            setGuide(data)
         }) 
     },[])
     return (
         <PageWrapper>
             <Container>
-                {list.guide.map(g=>(
+                <div key={guide && guide.id} style={{display:'flex', flexDirection:'column'}}>
+                    <span>{guide && guide.name}</span>
+                    <span>{guide && guide.age}</span>
+                    <span>{guide && guide.language}</span>
+                    <span>{guide && guide.transport}</span>
+                    <span>{guide && guide.quiantity}</span>
+                    <span>{guide && guide.about}</span>
+                    <span>{guide && guide.available}</span>
+                    <div>{guide && guide.tours.map(pack=>(
+                        <div key={pack.index}>
+                            <span>{pack.place}</span>
+                            <span>{pack.day}</span>
+                            <span>{pack.price}</span>
+                        </div>
+                    ))}</div>
                     <div>
-                        <span>{g.name}</span>
-                        <span>{g.age}</span>
+                        <span>{guide && guide.contacts.whatsapp} </span>
+                        <span>{guide && guide.contacts.telegram} </span>
+                        <span>{guide && guide.contacts.instagram} </span>
                     </div>
-                ))}
+
+                </div>
             </Container>
             
         </PageWrapper>
