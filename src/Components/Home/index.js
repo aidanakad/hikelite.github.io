f644719749e9932b01b055c09030870cec4cb864
aidanakad <aydana0504@gmail.com>
2020-07-17 12:36:22 +0600

    added home page

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import style from './home.module.css'
 import PageWrapper from '../PageWrapper'
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import {NavLink} from 'react-router-dom'
 
 export default function Home (){
     return (
         <div>
             <PageWrapper>
-                about us
+            <div className={style.bg}>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div className={style.info}>
+                                <h1 style={{marginBottom:'18px'}}>Start to explore marvelous Kyrgyzstan</h1>
+                                <h4 style={{marginBottom:'18px'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam!</h4>
+                                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nobis voluptate eaque facilis consectetur aspernatur.</h3>
+                                <NavLink to='/guides'>
+                                    <Button variant="success">Find a guide</Button>
+                                </NavLink>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             </PageWrapper>
         </div>
     )
