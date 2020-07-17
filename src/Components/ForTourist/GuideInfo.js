a6b56315537f649ae93dcef9d17b2bfb0062436d
aidanakad <aydana0504@gmail.com>
2020-07-17 14:29:12 +0600

    render seperate guide page

--- src/Components/ForTourist/GuideInfo.js
+++ src/Components/ForTourist/GuideInfo.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './guides.module.css'
 import PageWrapper from '../PageWrapper'
+import {Container} from 'react-bootstrap'
 
-export default function GuideInfo (){
+export default function GuideInfo ({match:{params:{id}}}){
+    const [guide, setGuide] = useState(null)
+    // const endpoint = 'http://localhost:1717'
+    useEffect(()=>{
+        fetch(`http://localhost:1717/guides/${id}`)
+        .then(response=>{
+            if(!response.ok) throw new Error ('guide not found')
+            return response.json()
+        })
+        .then((info)=>{
+            setGuide(info)
+        })
+    },[])
+
     return (
-        <div>
             <PageWrapper>
-                about us
+                <Container>
+                    <div key={guide && guide.id} style={{dispaly:'flex', flexDirection:'column'}}>
+                        <span>{guide && guide.name}</span>
+                        <span>{guide && guide.age}</span>
+                        <span>{guide && guide.languages}</span>
+                        <span>{guide && guide.transport}</span>
+                        <span>{guide && guide.quantuty}</span>
+                        <span>{guide && guide.about}</span>
+                        <span>{guide && guide.available}</span>
+
+                        <div>
+                            {guide && guide.tours.map(pack=>(
+                                <div>
+                                    <span>{pack.place}</span>
+                                    <span>{pack.day}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </Container>
             </PageWrapper>
-        </div>
     )
 }
