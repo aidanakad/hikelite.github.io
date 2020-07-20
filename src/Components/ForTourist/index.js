7e3115dde5cbd50743f2065b16d41cd335c48ca3
aidanakad <aydana0504@gmail.com>
2020-07-20 19:40:13 +0600

    styled guides page

--- src/Components/ForTourist/index.js
+++ src/Components/ForTourist/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './guides.module.css'
 import PageWrapper from '../PageWrapper'
 import { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import {NavLink} from 'react-router-dom'
 import placeholder from '../../img/placeholder.jpg'
 
@@ -24,22 +24,28 @@ export default function Guides (){
     return (
         <div>
             <PageWrapper>
-                <Container>
-                    <h2>Choose a guide and start your adventure</h2>
-                    {list.map(guide =>(
-                        <div key={guide.id} className={style.guides}>
-                            <NavLink to={`/guides/${guide.id}`}>
-                                <button>
-                                    <img src={guide.img || placeholder}  alt="avatar" className={style.avatar}/>
-                                </button>
-                            </NavLink>
-                            <div>
-                                <span>{guide.name}</span>
-                                <span>{guide.age}</span>
-                                <span>{guide.languages}</span>
-                            </div>
-                        </div>
-                    ))}
+                <Container className={style.container}>
+                    <Row>
+                        <Col xs={12} md={12}><h2>Choose a guide and start your adventure</h2></Col>
+                        {/* <Col xs={6} md={4} l={3}> */}
+                            {list.map(guide =>(
+                                <div key={guide.id} className={style.wrapper}>
+                                    <div className={style.link}>
+                                        <NavLink to={`/guides/${guide.id}`}>
+                                            <button>
+                                                <img src={guide.img || placeholder}  alt="avatar" className={style.avatar}/>
+                                            </button>
+                                        </NavLink>
+                                    </div>
+                                    <div className={style.intro}>
+                                        <span>{guide.name}</span>
+                                        <span>{guide.age}</span>
+                                        <span>{guide.languages}</span>
+                                    </div>
+                                </div>
+                            ))}
+                        {/* </Col> */}
+                    </Row>
                 </Container>
             </PageWrapper>
         </div>
