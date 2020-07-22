2b9662f800ef14241916aff9775ea646c441ddc6
aidanakad <aydana0504@gmail.com>
2020-07-22 15:23:53 +0600

    Updates

---
+++ precache-manifest.2bba742ab82660ef0da628fcf753cc4d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd8df2e4b0c8c0d221136f7dba1a15b",
+    "url": "/academy-project/index.html"
+  },
+  {
+    "revision": "565b24100be3ed66f53b",
+    "url": "/academy-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c744307f22a50e5d159d",
+    "url": "/academy-project/static/css/main.43dcd41f.chunk.css"
+  },
+  {
+    "revision": "565b24100be3ed66f53b",
+    "url": "/academy-project/static/js/2.376973ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/academy-project/static/js/2.376973ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c744307f22a50e5d159d",
+    "url": "/academy-project/static/js/main.e4c90cec.chunk.js"
+  },
+  {
+    "revision": "cbe637476985a5e5b534",
+    "url": "/academy-project/static/js/runtime-main.7350dfdc.js"
+  },
+  {
+    "revision": "9c0d2a399dfb488411d9395f60387172",
+    "url": "/academy-project/static/media/bg.9c0d2a39.png"
+  },
+  {
+    "revision": "16c259f18595a9fc0d18f5bc6beb1e1d",
+    "url": "/academy-project/static/media/field.16c259f1.jpg"
+  },
+  {
+    "revision": "06f911c387b49cff493602d7dda57827",
+    "url": "/academy-project/static/media/man.06f911c3.jpg"
+  },
+  {
+    "revision": "2f49e232ff8c811e8ebd6047b73cca33",
+    "url": "/academy-project/static/media/mountains.2f49e232.png"
+  },
+  {
+    "revision": "6b3b9c4a4ae1916c11b22fd418aaec85",
+    "url": "/academy-project/static/media/woman.6b3b9c4a.jpg"
+  }
+]);
